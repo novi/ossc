016ed9816c09095c42239621ac43247177499b1f
Yusuke Ito <novi.mad@gmail.com>
2020-10-08 20:50:39 +0900

    add sync

--- rtl/nextasic/util.v
+++ rtl/nextasic/util.v
@@ -1,6 +1,22 @@
 `timescale 1ns/1ns
 `default_nettype none
 
+module FF2Sync(
+	input wire in,
+	input wire out_clk,
+	output wire out_data
+);
+
+	reg f1, f2;
+	
+	assign out_data = f2;
+	
+	always@ (out_clk) begin
+		f1 <= in;
+		f2 <= f1;
+	end
+endmodule
+
 module FF2SyncP(
 	input wire in,
 	input wire out_clk,
